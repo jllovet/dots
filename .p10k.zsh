62126fd6af14303a9a0bd8b16b79b40da0d678f3
Jonathan Llovet <jonathan.llovet@gmail.com>
2022-10-05 21:24:07 -0400

    Update prompt to only use one line

--- .p10k.zsh
+++ .p10k.zsh
@@ -96,20 +96,20 @@
     # vi_mode               # vi mode (you don't need this if you've enabled prompt_char)
     # vpn_ip                # virtual private network indicator
     # load                    # CPU load
-    disk_usage              # disk usage
+    # disk_usage              # disk usage
     ram                     # free RAM
     # swap                  # used swap
     todo                    # todo items (https://github.com/todotxt/todo.txt-cli)
     timewarrior             # timewarrior tracking status (https://timewarrior.net/)
     taskwarrior             # taskwarrior task count (https://taskwarrior.org/)
+    wifi                     # wifi speed
     time                    # current time
     # =========================[ Line #2 ]=========================
     newline
     # ip                    # ip address and bandwidth usage for a specified network interface
     # public_ip             # public IP address
     # proxy                 # system-wide http/https/ftp proxy
-    battery                 # internal battery
-    wifi                     # wifi speed
+    # battery                 # internal battery
     # example               # example user-defined segment (see prompt_example function below)
   )
 
